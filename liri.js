86afc758ea77d3f337d6ee512b47b062904cc3c4
Kenneth Barnes <kennethbarnes@Kenneths-MacBook-Air.local>
2018-04-12 17:49:27 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,38 @@
+```js
+require("dotenv").config();
+```
+ 
+ 
+ var queryURL = "https://www.npmjs.com/package/twitter";
+
+ $.ajax({
+   url: queryURL,
+   method: "GET"
+ }).then(updatePage);
+});
+
+
+$("#clear-all").on("click", clear);
+
+
+// build the query URL for the ajax request to the NYT API
+var queryURL = "https://www.npmjs.com/package/node-spotify-api)";
+
+// make the AJAX request to the API - GETs the JSON data at the queryURL.
+// the data then gets passed as an argument to the updatePage function
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(updatePage);
+});
+
+//  .on("click") function associated with the clear button
+$("#clear-all").on("click", clear);
+
+my-tweets`
+
+    * `spotify-this-song`
+
+    * `movie-this`
+
+    * `do-what-it-says`
